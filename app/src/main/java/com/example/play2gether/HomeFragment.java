56ff567b0d5ca005d568a9dbdd73ce8215bf2b02
Piotr601 <piotr.niedziolka601@gmail.com>
2022-05-28 12:57:59 +0200

    add click on first button

--- app/src/main/java/com/example/play2gether/HomeFragment.java
+++ app/src/main/java/com/example/play2gether/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.play2gether;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -17,20 +20,39 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
+
+    LinearLayout l1, l2, l3;
+    ImageView avatar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        ImageView avatar = (ImageView) view.findViewById(R.id.home_avatar);
+
+        // cardview slide
+        l1 = (LinearLayout) view.findViewById(R.id.l1);
+        l2 = (LinearLayout) view.findViewById(R.id.l2);
+        l3 = (LinearLayout) view.findViewById(R.id.l3);
+
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), testActivity.class);
+                in.putExtra("some","some data");
+                startActivity(in);
+            }
+        });
+
 
         // clicking on avatar
+        avatar = (ImageView) view.findViewById(R.id.home_avatar);
         avatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,16 +62,16 @@ public class HomeFragment extends Fragment{
         return view;
     }
 
+
+    // OTHER FUNCTIONS
     private void loadFragment(View view) {
-        if(view != null){
+        if (view != null) {
             FragmentTransaction fragmentTransaction = getActivity()
                     .getSupportFragmentManager().beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new SettingsFragment());
             fragmentTransaction.commit();
-        }
-        else {
+        } else {
             Toast.makeText(getActivity(), "Avatar Click Error", Toast.LENGTH_SHORT).show();
         }
     }
-
 }
